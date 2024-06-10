6b25503afda81b4e0bc8ccfeacafa02ca3236d60
Nurgali Nuraly <nurali.nurgali@gmail.com>
2024-06-10 23:17:41 +0500

    Added swiftlint

--- Shop/View Controllers/BasketViewController.swift
+++ Shop/View Controllers/BasketViewController.swift
@@ -8,26 +8,25 @@
 import UIKit
 
 final class BasketViewController: UIViewController {
-    
+
     private let dataManager = DataManager.shared
     private var tableView: UITableView!
     private var buyButton: UIButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         self.view.backgroundColor = .white
-        
+
         tableView = UITableView(frame: .zero, style: .plain)
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
-        
+
         NotificationCenter.default.addObserver(self, selector: #selector(refreshTableView), name: Notification.Name("RefreshTableView"), object: nil)
-        
-        
+
         buyButton = UIButton()
         buyButton.setTitle("Buy Now", for: .normal)
         buyButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
@@ -37,16 +36,14 @@ final class BasketViewController: UIViewController {
         buyButton.translatesAutoresizingMaskIntoConstraints = false
         buyButton.addTarget(self, action: #selector(buyButtonPressed), for: .touchUpInside)
         view.addSubview(buyButton)
-        
-        
-        
+
         NSLayoutConstraint.activate([
             // Constraints for UITableView
             tableView.topAnchor.constraint(equalTo: view.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: buyButton.topAnchor, constant: -10),
-            
+
             // Constraints for UIButton
             buyButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             buyButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
@@ -54,11 +51,11 @@ final class BasketViewController: UIViewController {
             buyButton.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
-    
+
     @objc func buyButtonPressed() {
-        
+
     }
-    
+
     @objc func refreshTableView() {
         tableView.reloadData()
     }
@@ -68,14 +65,13 @@ extension BasketViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dataManager.dataArray.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
+
         let product = dataManager.dataArray[indexPath.row]
         cell.textLabel?.text = product.title
-        
-        
+
         return cell
     }
 }
--- Shop/View Controllers/DetailedViewController.swift
+++ Shop/View Controllers/DetailedViewController.swift
@@ -8,15 +8,15 @@
 import UIKit
 
 final class DetailedViewController: UIViewController {
-    
+
     internal var product: Product?
     private var stackView: UIStackView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         view.backgroundColor = .white
-        
+
         guard let product = product else {
             imgView.image = UIImage(systemName: "basket")
             return
@@ -30,25 +30,25 @@ final class DetailedViewController: UIViewController {
         continueShopButton.setTitle("Положить в корзину", for: .normal)
         continueShopButton.layer.cornerRadius = 8
         continueShopButton.addTarget(self, action: #selector(continueShopButtonTapped), for: .touchUpInside)
-        
+
         stackView = UIStackView(arrangedSubviews: [continueShopButton, buyNowButton])
-        
+
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
         stackView.alignment = .fill
         stackView.spacing = 10
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        
+
         addViews()
     }
-    
+
     var imgView: UIImageView = {
         let imageView = UIImageView()
         imageView.backgroundColor = UIColor.white
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
-    
+
     let detailLabel: UILabel = {
         let label = UILabel()
         label.textColor = UIColor.black
@@ -57,7 +57,7 @@ final class DetailedViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    
+
     let nameLabel: UILabel = {
         let label = UILabel()
         label.textColor = UIColor.black
@@ -66,7 +66,7 @@ final class DetailedViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    
+
     let buyNowButton: UIButton = {
         let button = UIButton()
         button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
@@ -75,54 +75,49 @@ final class DetailedViewController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
-    
+
     let continueShopButton: UIButton = {
         let button = UIButton()
         button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
         button.tintColor = .blue
-        button.backgroundColor = .systemBlue    
+        button.backgroundColor = .systemBlue
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
-    
-    
-    
+
     func addViews() {
         view.addSubview(imgView)
         view.addSubview(nameLabel)
         view.addSubview(detailLabel)
         view.addSubview(stackView)
-        
-        
+
         let safeArea = view.safeAreaLayoutGuide
-        
-        
+
         imgView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 0).isActive = true
         imgView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: 16).isActive = true
         imgView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16).isActive = true
         imgView.widthAnchor.constraint(equalTo: safeArea.widthAnchor, constant: -16).isActive = true
         imgView.heightAnchor.constraint(equalTo: imgView.widthAnchor).isActive = true
-        
+
         nameLabel.topAnchor.constraint(equalTo: imgView.bottomAnchor, constant: 10).isActive = true
         nameLabel.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         nameLabel.heightAnchor.constraint(equalToConstant: 16
         ).isActive = true
         nameLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 8).isActive = true
         nameLabel.trailingAnchor.constraint(greaterThanOrEqualTo: safeArea.trailingAnchor, constant: 8).isActive = true
-        
+
         detailLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 10).isActive = true
         detailLabel.widthAnchor.constraint(equalTo: safeArea.widthAnchor).isActive = true
         detailLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 8).isActive = true
         detailLabel.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: 8).isActive = true
-        
-        
+
         stackView.topAnchor.constraint(greaterThanOrEqualTo: detailLabel.bottomAnchor, constant: 8).isActive = true
         stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
         stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
         stackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -16).isActive = true
-        
+
     }
-    
+
     @objc func buyNowButtonTapped() {
         if let tabBarController = self.tabBarController {
             DataManager.shared.dataArray.append(product!)
@@ -130,8 +125,7 @@ final class DetailedViewController: UIViewController {
             tabBarController.selectedIndex = 1
         }
     }
-    
-    
+
     @objc func continueShopButtonTapped() {
         if let navigationController = self.navigationController {
             DataManager.shared.dataArray.append(product!)
@@ -140,6 +134,3 @@ final class DetailedViewController: UIViewController {
         }
     }
 }
-
-
-
